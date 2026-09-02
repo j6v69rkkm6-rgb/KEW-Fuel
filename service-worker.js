6791c26c68240b5b22958d073ce702538d62f01f
j6v69rkkm6-rgb <j6v69rkkm6@privaterelay.appleid.com>
2026-09-02 15:28:33 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kew-fuel-master-colour-v7-clean-airtrail-1';
+const CACHE='kew-fuel-master-colour-v8-restored-1';
 const ASSETS=['./','./index.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
